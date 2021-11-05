d1d4407364b79f8712baf999c70f61942bf180a1
Shane Creedon <shane.creedon3@mail.dcu.ie>
2021-11-05 22:35:48 +0000

    main: - Add Launcher bash script. - Add .gitkeps for future directories.
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

---
+++ nw-application/src/main/java/com/nw/socialscore/OperationType.java
@@ -0,0 +1,7 @@
+package com.nw.socialscore;
+
+public enum OperationType {
+    ADD,
+    REMOVE,
+    EXPORT
+}
---
+++ nw-application/src/main/java/com/nw/socialscore/SocialScore.java
@@ -0,0 +1,23 @@
+package com.nw.socialscore;
+
+import java.math.BigDecimal;
+import java.net.URL;
+
+// Note: Lombok would be nice here to blueprint the class more succinctly.
+public class SocialScore {
+    private final URL url;
+    private final BigDecimal score;
+
+    public SocialScore(URL url, BigDecimal score) {
+        this.url = url;
+        this.score = score;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+}
